b878cd3d13dca06a79ac3a517a36f79904f83ede
Zhang Yuan <zhangyuan@ZhangdeMacBook-Air.local>
2018-04-14 14:29:37 +0800

    update

--- 20180414block-run/index.js
+++ 20180414block-run/index.js
@@ -9,34 +9,105 @@ const pool  = mysql.createPool({
     database        : 'test'
 });
 const port = 8088;
-const  queryPromise = (sql,bind=[]) =>{
+const getConn = ()=>{
     return new Promise((res,rej)=>{
-        pool.query(sql,bind,  (err, results, fields)=> {
-            if (error) rej (err);
-            res({result, field});
-        });
-    })
+        pool.getConnection(function(err, conn) {
+            if (err) rej (err);
+            res(conn);
+          });
+    });
 };
+const  queryPromise = (conn,sql,bind=[]) =>{
+    return new Promise((res,rej)=>{
+        conn.query(sql,bind,  (err, result, field)=> {
+            if (err) rej (err);
+            res(result);
+        });
+    });
+};
+
+let sqlCount = 1;
+async function sqlClear()
+{
+    let conn = await getConn();
+    await queryPromise(conn,'TRUNCATE TABLE test');
+    sqlCount = 0;
+}
+
+async function sqlCommon()
+{
+    let name = `test:${sqlCount}`;
+    let conn = await getConn();
+    let testSelete = await queryPromise(
+        conn,
+        'SELECT * FROM `test` WHERE `name`= ? AND `version`=?',
+        [name,sqlCount]
+    );
+    if(testSelete.length>0)
+    {
+        let testRes = await queryPromise(
+            conn,
+            'UPDATE `test` SET `name`= ?,`version`=?+1 WHERE `version`=?',
+            [name,sqlCount,sqlCount]
+        );
+        if(testRes.affectedRows>0)
+        {
+            sqlCount++;
+        }
+    } else {
+        console.log(sqlCount)
+    }
+}
+
+async function sqlTrans()
+{
+    let conn = await getConn();
+    await queryPromise(conn,'TRUNCATE TABLE test');
+}
+
+async function sqlBlock()
+{
+    let conn = await getConn();
+    await queryPromise(conn,'TRUNCATE TABLE test');
+}
+
 http.createServer( async (request, response) => {
+    try {
     let pathname = url.parse(request.url).pathname;
     //console.log(`url:http://127.0.0.1:${port}{$pathname}`)
+    let showText = 'test';
     switch (pathname)
     {
         case '/clear':
-            await queryPromise('TRUNCATE TABLE');
+            await sqlClear();
+            showText = 'clear';
             break;
         case '/common':
+            await sqlCommon();
+            showText = 'common';
             break;
         case '/trans':
+            await sqlTrans();
+            showText = 'trans';
             break;
         case '/block':
+            await sqlBlock();
+            showText = 'block';
             break;
     }
     response.writeHead(200, {'Content-Type': 'text/html'});    
-    response.write('test');
+    response.write(showText);
     response.end();
+    } catch(e) {
+        console.log(e.stack)
+    }
  }).listen(port);
- console.log(`url:http://127.0.0.1:${port} listening port:${port}`);
+ let abN = 100;
+ let abC = 10;
+ console.log(`clear url:http://127.0.0.1:${port}/clear`);
+ console.log(`ab common command: ab -n ${abN} -c ${abC} http://127.0.0.1:${port}/common`);
+ console.log(`ab trans command: ab -n ${abN} -c ${abC} http://127.0.0.1:${port}/trans`);
+ console.log(`ab block command: ab -n ${abN} -c ${abC} http://127.0.0.1:${port}/block`);
 
 
   
--- 20180414block-run/test.sql
+++ 20180414block-run/test.sql
@@ -1,6 +1,10 @@
 CREATE DATABASE IF NOT EXISTS `test`;
-CREATE TABLE `test`.`test` (
+CREATE TABLE  IF NOT EXISTS `test`.`test` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(32) DEFAULT NULL,
+  `version` int(11) DEFAULT '0',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+TRUNCATE TABLE `test`.`test`;
+INSERT INTO `test`.`test` (`id`,`name`,`version`) 
+VALUES (1,"test:1",1);
