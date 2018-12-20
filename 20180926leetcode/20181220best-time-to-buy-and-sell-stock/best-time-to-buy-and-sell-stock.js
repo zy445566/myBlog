0a2e998fa1d5f2f93a8c65ac24dd042649721c77
ZhangYuan <zyct@vip.qq.com>
2018-12-20 10:49:56 +0800

    add 20181220best-time-to-buy-and-sell-stock

---
+++ 20180926leetcode/20181220best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.js
@@ -0,0 +1,46 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+    // 优化头
+    while (prices.length>0) {
+        let maxPrice = Math.max.apply(this,prices);
+        if (maxPrice<=prices[0]) {
+            prices.shift();
+        } else {
+            break;
+        }
+    }
+    // 优化尾
+    while (prices.length>0) {
+        if (prices[prices.length-1]==0) {
+            prices.pop();
+        } else {
+            break;
+        }
+    }
+    // 开始动态规划填表
+    let dpTable = [];
+    for (let i = 0;i<prices.length;i++) {
+        dpTable[i] = [];
+        for (let j = 0;j<prices.length;j++) {
+            if (j<=i) {
+                dpTable[i].push('x')
+            } else {
+                dpTable[i].push(prices[j]-prices[i]);
+            }
+        }
+    }
+    // 开始动态规划求值
+    let maxValue = 0;
+    for (let i = 0;i<dpTable.length;i++) {
+        for (let j = i+1;j<dpTable[i].length;j++) {
+            if (dpTable[i][j]=='x') {continue;}
+            if (dpTable[i][j]>maxValue) {
+                maxValue = dpTable[i][j];
+            }
+        }
+    }
+    return maxValue;
+};
--- 20180926leetcode/README.md
+++ 20180926leetcode/README.md
@@ -1,3 +1,4 @@
 # leetcode
 * [三数之和的优化之旅(20180926)](./20180926three-sum/README.md)
 * [解数独(20181023)](./20181023sudoku-solver/README.md)
+* [用买卖股票的最佳时机理解动态规划(20181220)](./20181220best-time-to-buy-and-sell-stock/README.md)
